7494c3be98cd7055404ab60cf6638a5dffdc9335
kino <kinoxyz1@gmail.com>
2023-03-28 18:49:37 +0800

    冒泡排序(递归-跳过有序记录)

--- algorithm/src/main/java/com/kino/algorithm/d_sort/BubbleSort.java
+++ algorithm/src/main/java/com/kino/algorithm/d_sort/BubbleSort.java
@@ -10,14 +10,14 @@ public class BubbleSort {
 
     /**
      * 写法一: 普通方法
-     *   双层for:
-     *     外层 for 控制循环次数
-     *     内层 for 控制元素和后一个元素相比较
+     * 双层for:
+     * 外层 for 控制循环次数
+     * 内层 for 控制元素和后一个元素相比较
      *
      * @param array
      * @return
      */
-    public static int[] BubbleSort1(int[] array) {
+    public static int[] bubbleSort1(int[] array) {
         for (int i = 0; i < array.length; i++) { // 控制循环次数
             for (int j = 0; j < array.length - 1; j++) {  // 比较
                 if (array[j] > array[j + 1]) {  // 条件成立交换位置
@@ -37,7 +37,7 @@ public class BubbleSort {
      * @param right 需要比较的数据的右边界
      * @return 数组
      */
-    public static int[] BubbleSort2(int[] array, int right) {
+    public static int[] bubbleSort2(int[] array, int right) {
         if (right == 0) {
             return array;
         }
@@ -48,7 +48,32 @@ public class BubbleSort {
                 array[i + 1] = tmp;
             }
         }
-        BubbleSort2(array, --right);
+        bubbleSort2(array, --right);
+        return array;
+    }
+
+    /**
+     * 写法三: 递归实现 plus
+     * 在写法二中, 如果数组前面几个元素已经是有序的, 那不管怎么样也会再去比较一次, 这里对这部分进行改进.
+     *
+     * @param array
+     * @param right
+     * @return
+     */
+    public static int[] bubbleSort3(int[] array, int right) {
+        if (right == 0) {
+            return array;
+        }
+        int boundary = 0; // 打记号, 如果发生了交换, 就把被交换记录的index给boundary用于下一次递归
+        for (int i = 0; i < right; i++) {
+            if (array[i] > array[i + 1]) {
+                int tmp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = tmp;
+                boundary = i;
+            }
+        }
+        bubbleSort3(array, boundary);
         return array;
     }
 }
--- algorithm/src/test/java/com/kino/algorithm/d_sort/BubbleSortTest.java
+++ algorithm/src/test/java/com/kino/algorithm/d_sort/BubbleSortTest.java
@@ -12,9 +12,11 @@ public class BubbleSortTest {
     @Test
     public void test1() {
         int[] array = {12,1,5,2,9,6};
-        System.out.println(Arrays.toString(BubbleSort.BubbleSort1(array)));
+        System.out.println(Arrays.toString(BubbleSort.bubbleSort1(array)));
 
-        System.out.println(Arrays.toString(BubbleSort.BubbleSort2(array, array.length-1)));
+        System.out.println(Arrays.toString(BubbleSort.bubbleSort2(array, array.length-1)));
+
+        System.out.println(Arrays.toString(BubbleSort.bubbleSort3(array, array.length-1)));
 
 
     }
