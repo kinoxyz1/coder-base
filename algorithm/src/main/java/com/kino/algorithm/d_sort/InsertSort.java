f1bd1101b249ccd164bb2c25f49241518130a2bf
kino <kinoxyz1@gmail.com>
2023-03-30 23:02:34 +0800

    插入排序(部分)

--- algorithm/src/main/java/com/kino/algorithm/d_sort/InsertSort.java
+++ algorithm/src/main/java/com/kino/algorithm/d_sort/InsertSort.java
@@ -10,6 +10,10 @@ import java.util.Arrays;
  */
 public class InsertSort {
 
+    /**
+     * 往后移版本
+     * @param array
+     */
     public static void sort1(int[] array) {
         if (null == array || array.length == 1) {
             return;
@@ -20,23 +24,47 @@ public class InsertSort {
         int bound = 1;
         for (int i = bound; i < array.length; i++) { // 外层for遍历所有无序元素
             // 内层for取 第一个无序元素 和所有有序元素相比较, 如果可以, 则交换位置
-            int changeIndex = -1; //记录最后发生移动元素的index
+            int changeIndex = -1; // 记录最后发生移动元素的index
             int compare = array[i]; // 第一个无序元素
             for (int j = bound - 1; j >= 0; j--) {
-                if(compare < array[j]){ // 无序元素比前一个元素小, 则将前一个元素后移
-                    array[j+1] = array[j];
+                if (compare < array[j]) { // 无序元素比前一个元素小, 则将前一个元素后移
+                    array[j + 1] = array[j];
                     changeIndex = j;
                 }
             }
-            if(changeIndex != -1){
+            if (changeIndex != -1) {
                 array[changeIndex] = compare;
             }
             bound++;
         }
     }
 
+    /**
+     * 交换顺序版本
+     * @param array
+     */
+    public static void sort2(int[] array) {
+
+    }
+
+    /**
+     * 递归版本
+     * @param array
+     */
+    public static void sort3(int[] array) {
+
+    }
+
+    /**
+     * 二分法版本
+     * @param array
+     */
+    public static void sort4(int[] array) {
+
+    }
+
     public static void main(String[] args) {
-        int[] array = {65,232,65,23,674,367,136,2,54,64,309,92};
+        int[] array = {65, 232, 65, 23, 674, 367, 136, 2, 54, 64, 309, 92};
         sort1(array);
         System.out.println(Arrays.toString(array));
     }
