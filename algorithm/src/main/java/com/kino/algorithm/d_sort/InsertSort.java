1584a5509bfd8741ab71267a4226631c43d1ef5b
kino <kinoxyz1@gmail.com>
2023-03-31 16:35:39 +0800

    插入排序(递归)

--- algorithm/src/main/java/com/kino/algorithm/d_sort/InsertSort.java
+++ algorithm/src/main/java/com/kino/algorithm/d_sort/InsertSort.java
@@ -12,6 +12,7 @@ public class InsertSort {
 
     /**
      * 往后移版本
+     *
      * @param array
      */
     public static void sort1(int[] array) {
@@ -30,6 +31,7 @@ public class InsertSort {
                 if (compare < array[j]) { // 无序元素比前一个元素小, 则将前一个元素后移
                     array[j + 1] = array[j];
                     changeIndex = j;
+                    // 考虑有重复元素,所以没有直接break;
                 }
             }
             if (changeIndex != -1) {
@@ -40,32 +42,48 @@ public class InsertSort {
     }
 
     /**
-     * 交换顺序版本
+     * 递归版本
+     *
      * @param array
      */
     public static void sort2(int[] array) {
-
+        sort2(array, 1);
     }
 
     /**
-     * 递归版本
+     * @param array 数组
+     * @param bound 界限, 将数组分为已排序和未排序两部分, bound 是未排序的第一个元素
+     */
+    private static void sort2(int[] array, int bound) {
+        if (array == null || array.length < 2 || bound == array.length) {
+            return;
+        }
+        int compare = array[bound]; // 无序中代比较的第一个元素
+        int index = bound - 1; // 有序的最后一个元素下标
+        while (index >= 0 && compare < array[index]) {
+            array[index + 1] = array[index];
+            index--;
+        }
+        if (index + 1 != bound) {
+            array[index + 1] = compare;
+        }
+        sort2(array, ++bound);
+    }
+
+    /**
+     * 二分法版本
+     *
      * @param array
      */
     public static void sort3(int[] array) {
 
     }
 
-    /**
-     * 二分法版本
-     * @param array
-     */
-    public static void sort4(int[] array) {
-
-    }
-
     public static void main(String[] args) {
-        int[] array = {65, 232, 65, 23, 674, 367, 136, 2, 54, 64, 309, 92};
-        sort1(array);
+        int[] array = {232, 61, 65, 23, 674, 367, 136, 2, 54, 64, 309, 92};
+        // sort4(array);
+        // sort2(array);
+        sort3(array);
         System.out.println(Arrays.toString(array));
     }
 }
