1809b8aec24475fa9c8eb86ee25d3ed524e53c4c
kino <kinoxyz1@gmail.com>
2023-04-20 00:22:48 +0800

    leetcode83 单、双指针的写法

---
+++ algorithm/src/main/java/com/kino/algorithm/c_linked/Leetcode83.java
@@ -0,0 +1,48 @@
+package com.kino.algorithm.c_linked;
+
+/**
+ * @author kino
+ * @date 2023/4/20 00:00
+ */
+public class Leetcode83 {
+
+    // 单指针
+    public static ListNode deleteDuplicates1(ListNode head) {
+        if (null == head || head.next == null) {
+            return head;
+        }
+        ListNode s = new ListNode(-999, head);
+        ListNode p1 = s.next;
+        while (p1 != null && p1.next != null) {
+            if (p1.val == p1.next.val) {
+                p1.next = p1.next.next;
+            } else {
+                p1 = p1.next;
+            }
+        }
+        return s.next;
+    }
+
+    // 双指针
+    public static ListNode deleteDuplicates(ListNode head){
+        if (null == head || head.next == null) {
+            return head;
+        }
+        ListNode s = new ListNode(-999, head);
+        ListNode p1 = s;
+        ListNode p2;
+        while ((p2 = p1.next) != null) {
+            if (p1.val == p2.val) {
+                p1.next = p2.next;
+            } else {
+                p1 = p1.next;
+            }
+        }
+        return s.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode of = ListNode.of(-1,0,0,0,0,3,3);
+        System.out.println(deleteDuplicates(of));
+    }
+}
