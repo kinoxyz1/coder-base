c972d771a498e6185fe61a5b75c53199a285fad4
kino <kinoxyz1@gmail.com>
2023-04-23 15:20:22 +0800

    leetcode23

---
+++ algorithm/src/main/java/com/kino/algorithm/c_linked/Leetcode23.java
@@ -0,0 +1,73 @@
+package com.kino.algorithm.c_linked;
+
+/**
+ * 给你一个链表数组，每个链表都已经按升序排列。
+ * <p>
+ * 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+ * <p>
+ * 示例 1：
+ * 输入：lists = [[1,4,5],[1,3,4],[2,6]]
+ * 输出：[1,1,2,3,4,4,5,6]
+ * 解释：链表数组如下：
+ * [
+ * 1->4->5,
+ * 1->3->4,
+ * 2->6
+ * ]
+ * 将它们合并到一个有序链表中得到。
+ * 1->1->2->3->4->4->5->6
+ * <p>
+ * 示例 2：
+ * 输入：lists = []
+ * 输出：[]
+ * <p>
+ * 示例 3：
+ * 输入：lists = [[]]
+ * 输出：[]
+ * 提示：
+ * <p>
+ * k == lists.length
+ * 0 <= k <= 10^4
+ * 0 <= lists[i].length <= 500
+ * -10^4 <= lists[i][j] <= 10^4
+ * lists[i] 按 升序 排列
+ * lists[i].length 的总和不超过 10^4
+ *
+ * @author kino
+ * @date 2023/4/23 14:16
+ */
+public class Leetcode23 {
+    public static ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) return null;
+        return split(lists, 0, lists.length - 1);
+    }
+
+    public static ListNode split(ListNode[] lists, int i, int j) {
+        if (i == j) return lists[i];
+        int step = (i + j) >>> 1;
+        ListNode left = split(lists, i, step);
+        ListNode right = split(lists, step+1, j);
+        return mergeTwoLists(left, right);
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (null == list1) return list2;
+        if (null == list2) return list1;
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode[] lists = {
+                ListNode.of(1, 4, 5),
+                ListNode.of(1, 3, 4),
+                ListNode.of(2, 6)
+        };
+        System.out.println(mergeKLists(lists));
+    }
+}
