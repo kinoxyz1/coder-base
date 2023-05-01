66af416e5bf675d0d7b73b2ff004e221429b382a
kino <kinoxyz1@gmail.com>
2023-05-01 22:54:07 +0800

    leetcode141 and leetcode142

---
+++ algorithm/src/main/java/com/kino/algorithm/c_linked/Leetcode141.java
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kino.algorithm.c_linked;
+
+/**
+ * @author kino
+ * @date 2023/4/24 11:48
+ */
+public class Leetcode141 {
+    public static boolean hasCycle(ListNode head) {
+        if (null == head || head.next == null) return false;
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+
+    public static ListNode hasCycleFindStart(ListNode head) {
+        if (null == head || head.next == null) return null;
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                slow = head;
+                while (true) {
+                    if (slow == fast) return slow;
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        ListNode n12 = new ListNode(12, null);
+        ListNode n11 = new ListNode(11, n12);
+        ListNode n10 = new ListNode(10, n11);
+        ListNode n9 = new ListNode(9, n10);
+        ListNode n8 = new ListNode(8, n9);
+        ListNode n7 = new ListNode(7, n8);
+        ListNode n6 = new ListNode(6, n7);
+        ListNode n5 = new ListNode(5, n6);
+        ListNode n4 = new ListNode(4, n5);
+        ListNode n3 = new ListNode(3, n4);
+        ListNode n2 = new ListNode(2, n3);
+        ListNode n1 = new ListNode(1, n2);
+
+        n12.next = n1;
+
+        System.out.println(hasCycleFindStart(n1).val);
+    }
+}
